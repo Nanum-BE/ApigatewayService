205b998d14c1f11a5f7c5bfd460835c96df7fd86
Chan <33cks1423@naver.com>
2022-09-26 15:56:16 +0900

    first

---
+++ src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -0,0 +1,75 @@
+package com.example.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+    public LoggingFilter(){
+        super(Config.class);
+
+    }
+
+    @Override
+    public GatewayFilter apply(LoggingFilter.Config config) {
+        // Pre Filter 커스텀 하기
+//        return (exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//            ServerHttpResponse response = exchange.getResponse();
+//            log.info("Global Filter baseMessage:/·// {}", config.getBaseMessage());
+//
+//            if(config.isPreLogger()){
+//                log.info("Global Filter Start: request id -> {}",request.getId());
+//            }
+//            // Custom Post Filter
+////            Mono는 0-1개의 결과만을 처리하기 위한 Reactor 객체
+////            Flux는 0-N개의 결과물을 처리하기 위한 Reactor 객체
+//
+//            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+//                if(config.isPostLogger()) {
+//                    log.info("Global POST filter: response code -> {}", response.getStatusCode());
+//                }
+//            }));
+//        };
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain)->{
+                        ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("Logging Filter baseMessage:/·// {}", config.getBaseMessage());
+
+            if(config.isPreLogger()){
+                log.info("Logging Filter Start: request id -> {}",request.getId());
+            }
+            // Custom Post Filter
+//            Mono는 0-1개의 결과만을 처리하기 위한 Reactor 객체
+//            Flux는 0-N개의 결과물을 처리하기 위한 Reactor 객체
+
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                if(config.isPostLogger()) {
+                    log.info("Logging POST filter: response code -> {}", response.getStatusCode());
+                }
+            }));
+        }, Ordered.LOWEST_PRECEDENCE);
+
+//        순서 적용
+//        Ordered.HIGHEST_PRECEDENCE 가장 높음
+        return filter;
+    }
+    @Data
+    public static class Config{
+        /// configuration properties 넣기
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+        private String sayHello;
+    }
+}
