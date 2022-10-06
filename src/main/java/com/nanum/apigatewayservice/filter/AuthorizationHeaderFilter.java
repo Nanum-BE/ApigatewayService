4b1d280fd93e8989e05fb0ab71574473e1ad5e22
mins00oo <alstn7223@gmail.com>
2022-10-06 10:36:55 +0900

    sms Path 수정

--- src/main/java/com/nanum/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/nanum/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
